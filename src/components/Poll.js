a2470fbda520feabf9db261ba34187fdd3f82951
Teddy Leung <teddy.ph.leung@gmail.com>
2019-10-17 16:52:49 -0700

    Installed socket.io-client and built Poll component to connect.

--- src/components/Poll.js
+++ src/components/Poll.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import openSocket from 'socket.io-client';
 
-const Poll = props => {
-  return (
-    <h1>Polls</h1>
-  );
+class Poll extends Component {
+  constructor(props) {
+    super(props);
+    this.socket = openSocket('http://localhost:5000');
+
+    this.socket.on('connected', data => {
+      console.log(data);
+    });
+  
+    this.socket.on('poll', data => {
+      console.log(data);
+    });
+  };
+
+  handleClick = () => {
+    this.socket.emit('answer', 'a');
+  }
+
+  componentWillUnmount() {
+    this.socket.close();
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Polls</h1>
+        <button onClick={this.handleClick}>A</button>
+      </>
+    );
+  }
 };
 
 export default Poll;
